720debc772e85d63e721d60d345173d4780e3d26
silent97vvaaavava <31998878+silent97vvaaavava@users.noreply.github.com>
2019-02-05 10:40:48 +0200

    Add files via upload

--- application/views/main/about.php
+++ application/views/main/about.php
@@ -1,1 +1,19 @@
-
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Драсьте</h1>
+                    <span class="subheading">Пытаюсь долго решить одну проблему</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Собсна проблему решил. Не стоило мне ставить сервер самому, а воспользоваться аля Denwer или что-то вроде того. А счастье было так близко... </p>
+        </div>
+    </div>
+</div>
